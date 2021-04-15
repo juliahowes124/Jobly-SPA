2764055e182e246d05ee2c3db890b78570efaa97
Kevin Huang <kehuang805@gmail.com>
2021-04-15 14:50:32 -0700

    installed lodash and implemented live search for SearchBar component

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,28 +1,36 @@
-import React, {useState} from 'react';
+/* Live search reference:
+* https://medium.com/@mikjailsalazar/just-another-searchbar-react-axios-lodash-340efec6933d
+*/
+
+import React, {useState, useCallback} from 'react';
 import JoblyApi from './api';
+import _ from "lodash";
+
 
 function SearchBar({handleFilter, filterType}) {
-  const [searchTerm, setSearchTerm] = useState('')
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const filter = useCallback(
+    async function filter(searchTerm) {
+      if(filterType === "companies") {
+        let companies = await JoblyApi.getCompanies(searchTerm);
+        handleFilter(companies);
+    } else {
+        let jobs = await JoblyApi.getJobs(searchTerm);
+        handleFilter(jobs);
+    }}, [filterType, handleFilter]);
+
+  const search = useCallback(_.debounce(filter, 1000), [filter]); // eslint-disable-line react-hooks/exhaustive-deps
 
   function handleChange(evt) {
+    console.log("handleChange");
     setSearchTerm(evt.target.value);
-  }
-
-  async function filter(evt) {
-    evt.preventDefault();
-    if(filterType === "companies") {
-      let companies = await JoblyApi.getCompanies(searchTerm);
-      handleFilter(companies);
-    } else {
-      let jobs = await JoblyApi.getJobs(searchTerm);
-      handleFilter(jobs);
-    }
+    search(evt.target.value);
   }
 
   return (
-    <form onSubmit={filter}>
-      <input onChange={handleChange} value={searchTerm}/>
-      <button>Submit</button>
+    <form>
+      <input onChange={handleChange} value={searchTerm} placeholder="Enter search term"/>
     </form>
   )
 
